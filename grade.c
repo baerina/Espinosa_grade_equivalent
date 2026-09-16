59baec37d80d3f3e63bff33b1d8e3d72d2a5440b
Ninya <118419029+baerina@users.noreply.github.com>
2026-09-16 14:51:27 +0800

    Create grade.c

---
+++ grade.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+  int main() {
+    //declare the given first
+    int grade;
+    int score range;
+    char A, B, C, D, F;
+    
+//input: ask the user for the input, which is grade
+    printf("Enter your score: ");
+    scanf("%i" ,&grade);
+    
+    if (grade <= 100) {
+      printf("A\n");
+    } else {
+      
+
+
+//process: determine what letter grade is the input
+
+
+//output: display letter grade
